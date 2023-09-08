0a14653c35e83d4f9248fa8165870f953db94894
Rui Cardoso <ruipepeicar@gmail.com>
2023-09-08 22:49:44 +0100

    bug fix

--- examples/train_curriculum.py
+++ examples/train_curriculum.py
@@ -24,7 +24,7 @@ SUB_DIRS = ["task_type_1_2", "task_type_3_GR_Platform_HC",
             "task_type_4_GR_Platform_FA"]
 # N_TASKS = [108, 108, 1404, 1080, 2808, 2916, 32292, 57996]
 N_TASKS = [216, 1404, 1080]
-REPS = [1, 1, 1]
+REPS = [4, 1, 1]
 TASK_FILE = "all_tasks.yml"
 
 
