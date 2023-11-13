de50f1226a13e3030c6bff60bdec748faabd415f
Rui Cardoso <ruipepeicar@gmail.com>
2023-11-13 13:17:15 +0000

    resets optimiser after training epoch to avoid local minima

--- animalai/animalai/envs/alearner_e2e.py
+++ animalai/animalai/envs/alearner_e2e.py
@@ -74,7 +74,7 @@ class ALearnerE2E():
                             ))
 
         self.optimiser = th.optim.Adam(self.aler.parameters(), lr=0.001,
-                                       weight_decay=1e-3)
+                                       weight_decay=1e-5)
         # self.optimiser = th.optim.SGD(self.aler.parameters(), lr=0.01,
         #                               momentum=0.9, nesterov=True)
         # self.criterion = nn.MSELoss()
@@ -82,7 +82,7 @@ class ALearnerE2E():
 
     def reset_optimiser(self):
         self.optimiser = th.optim.Adam(self.aler.parameters(), lr=0.001,
-                                       weight_decay=1e-3)
+                                       weight_decay=1e-5)
         # self.optimiser = th.optim.SGD(self.aler.parameters(), lr=0.01,
         #                               momentum=0.9, nesterov=True)
         self.n_epochs = 10
